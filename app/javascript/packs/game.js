60039d65dac2e976d82dd93c95c82c4a678c10a2
saitoizumi <ilrw3110123@gmail.com>
2021-07-22 22:01:23 +0900

    add: 制限時間でのクリック回数取得

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -1,3 +1,4 @@
+import { nodeName } from 'jquery';
 import Swal from 'sweetalert2'
 
 Swal.fire({
@@ -24,7 +25,6 @@ function countdown(setTime, id) {
   let text = `<div class="countdown-number">${remainSec}</div>`;
 
   if (diffSec <= 0) {
-    // セットしたタイマーを解除
     clearInterval(timeId);
     text = `<div class="start-fadeout">スタート</div>`;
     countdownSec(10000, '#sec');
@@ -35,24 +35,30 @@ function countdown(setTime, id) {
 
 function countdownSec(setTime, id) {
   let totalTime = setTime;
-  // 読み込まれた最初の時間
   const oldTime = Date.now();
-  // タイマーをセット
+
   const timeId = setInterval(() => {
-  // 読み込まれ続ける時間
   const currentTime = Date.now();
 
   const diff = currentTime - oldTime;
   const diffSec = totalTime - diff;
-  // Math.ceil()関数は引数として与えた数以上の最小の整数を返す
+
   const remainSec = Math.ceil(diffSec / 1000);
   let text = `残り${remainSec}秒`;
 
+  const clickButton = document.querySelector('.click-button')
+  clickButton.addEventListener('click', clickFrequency)
+
   if (diffSec <= 0) {
-    // セットしたタイマーを解除
     clearInterval(timeId);
     text = "終了";
-  }
+    clickButton.style['pointer-events'] = 'none';
+    }
   document.querySelector(id).innerHTML = text;
   });
 }
+
+function clickFrequency() {
+  console.log("クリック");
+}
+ 
