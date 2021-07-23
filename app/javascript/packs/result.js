602f0119e861afbb741675aa61acb4fc0025b9ec
saitoizumi <ilrw3110123@gmail.com>
2021-07-24 00:42:32 +0900

    update: url変更

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -3,6 +3,6 @@ let text = `${score}cm`;
 document.querySelector('#score').innerHTML = text;
 
 const link = document.querySelector('.twitter-share');
-const url = `https://twitter.com/share?&url=https://otsumamiko.app/&text=${score}cmのかき氷を作りました！&hashtags=かき氷作り,かき氷メーカー&lang=ja`;
+const url = `https://twitter.com/share?&url=https://kakigoori-tower.herokuapp.com/&text=${score}cmのかき氷を作りました！&hashtags=かき氷タワー,かき氷好きと繋がりたい&lang=ja`;
 
 link.setAttribute('href', url);
