1d60c30d8c09c99f4cd9b3a9f8f38596ee3537d6
saitoizumi <ilrw3110123@gmail.com>
2021-07-24 16:57:19 +0900

    update: コメント修正

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -7,25 +7,37 @@ const url = `https://twitter.com/share?&url=https://kakigoori-tower.herokuapp.co
 
 link.setAttribute('href', url);
 
-if (text == "nullcm") {
+if (score == "nullcm") {
   let text = `先に結果を見るなんて...さすがエンジニアです。`;
   document.querySelector('#comment').innerHTML = text;
-} else if (text < "20cm") {
+} else if (0 <= score && score < 20) {
   let text = `修行が足りません。出直してください。`;
   document.querySelector('#comment').innerHTML = text;
-} else if ("20cm" <= text && text < "40cm") {
+} else if (20 <= score && score < 40) {
   let text = `まあまあな結果です。`;
   document.querySelector('#comment').innerHTML = text;
-} else if ("40cm" <= text && text < "60cm") {
+} else if (40 <= score && score < 60) {
   let text = `良い感じです！`;
   document.querySelector('#comment').innerHTML = text;
-} else if ("60cm" <= text && text < "80cm") {
+} else if (60 <= score && score < 80) {
   let text = `すごいです！なかなか手が痛いことでしょう。`;
   document.querySelector('#comment').innerHTML = text;
-} else if ("80cm" <= text && text < "90cm") {
-  let text = `半端ないです！!これは1位を狙えるのでは！？`;
+} else if (80 <= score && score < 90) {
+  let text = `半端ないです！!でも1位までまだまだですね。`;
   document.querySelector('#comment').innerHTML = text;
-} else if ("90cm" <= text) {
-  let text = `もしや新記録更新！？`;
+} else if (90 <= score && score < 100) {
+  let text = `もうすぐで大台です！`;
+  document.querySelector('#comment').innerHTML = text;
+} else if (100 <= score && score < 110) {
+  let text = `想定以上です。あなたの指は何本あるんですか！？`;
+  document.querySelector('#comment').innerHTML = text;
+} else if (110 <= score && score < 120) {
+  let text = `想定以上で作者が震えています。コツのシェアをお願いします...`;
+  document.querySelector('#comment').innerHTML = text;
+} else if (120 <= score && score < 130) {
+  let text = `これは最高記録では!?1?あなたは何者...`;
+  document.querySelector('#comment').innerHTML = text;
+} else if (130 <= score ) {
+  let text = `レベチ過ぎてついていけません。これはもはやあなたのアプリ...`;
   document.querySelector('#comment').innerHTML = text;
 }
